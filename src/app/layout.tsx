2dd206d41a1bb91fec0997987309f045bdc71548
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-08-12 17:06:17 +0500

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Wrapper>
-        {/* <Navbar/> */}
+        <Navbar/>
         {children}
-        {/* <Footer/> */}
+        <Footer/>
         </Wrapper>
         </body>
     </html>
