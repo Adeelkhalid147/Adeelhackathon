6ba70e2d855a2c05a32e1ad2a335ad8d61128db3
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-08-15 17:53:50 +0500

    ise solve

--- src/components/views/Navbar/subComponents/Cartstate.tsx
+++ src/components/views/Navbar/subComponents/Cartstate.tsx
@@ -1,18 +1,26 @@
 import { cartContext } from "@/global/Context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CgShoppingCart } from "react-icons/cg";
 
 const Cartstate = () => {
-  let data = localStorage.getItem("Cart") as string;
+  const [quantity, setQuantity] = useState(0)
+  const isBrowser = () => typeof window !== undefined
+  useEffect(()=> {
+    let data = localStorage.getItem("Cart") as string;
+    setQuantity(JSON.parse(data).length)
+  },[])
 
+  if (isBrowser()) {
   return (
     <div className="flex-shrink bg-[#F1F1F1] relative rounded-full h-10 w-10 flex justify-center items-center">
       <div className="absolute bg-[#c86062] h-3 w-3 text-xs rounded-full flex justify-center items-center top-1 right-1">
-        {/* {JSON.parse(data).length} */}
+        {quantity}
       </div>
       <CgShoppingCart size={25} />
     </div>
   );
-};
-
+} else {
+  return <p>loading</p>
+}
+}
 export default Cartstate;
