c8d25c361c1d6132e7282443820f58f57d1d3f3b
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-08-15 17:39:43 +0500

    cart

--- src/components/views/Navbar/subComponents/Cartstate.tsx
+++ src/components/views/Navbar/subComponents/Cartstate.tsx
@@ -1,18 +1,18 @@
-import { cartContext } from '@/global/Context'
-import { useContext } from 'react'
-import { CgShoppingCart } from "react-icons/cg"
+import { cartContext } from "@/global/Context";
+import { useContext } from "react";
+import { CgShoppingCart } from "react-icons/cg";
 
 const Cartstate = () => {
-    let {state} = useContext(cartContext)
-    console.log("adeel state :",state)
-  return (
-    <div className='flex-shrink bg-[#F1F1F1] relative rounded-full h-10 w-10 flex justify-center items-center'>
-    <div className='absolute bg-[#c86062] h-3 w-3 text-xs rounded-full flex justify-center items-center top-1 right-1'>
-      {state.cart.length}
-    </div>
-    <CgShoppingCart size={25}/>
-    </div>
-  )
-}
+  let data = localStorage.getItem("Cart") as string;
 
-export default Cartstate
+  return (
+    <div className="flex-shrink bg-[#F1F1F1] relative rounded-full h-10 w-10 flex justify-center items-center">
+      <div className="absolute bg-[#c86062] h-3 w-3 text-xs rounded-full flex justify-center items-center top-1 right-1">
+        {/* {JSON.parse(data).length} */}
+      </div>
+      <CgShoppingCart size={25} />
+    </div>
+  );
+};
+
+export default Cartstate;
